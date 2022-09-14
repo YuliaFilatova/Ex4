a27b397ed5914053822340d5c3dd5d49fce98c7c
Yuliya <filatovajulia@rambler.ru>
2022-09-14 23:31:13 +0300

    add Ex3

---
+++ Ex3/Program.cs
@@ -0,0 +1,20 @@
+﻿// Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран. Элементы массива вводятся пользователем.
+
+int[] myArray = new int[8];
+
+for (int i = 0; i < myArray.Length; i++)
+{
+   Console.WriteLine("Введите элемент массива (i = {0}): ", i);
+   myArray [i] = int.Parse(Console.ReadLine());
+}
+
+Console.WriteLine("Вывод массива" );
+for (int i = 0; i < myArray.Length; i++)
+{
+    Console.WriteLine(myArray[i]);
+}
+
+
+        
+
+
